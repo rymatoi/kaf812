5679f73bbc0ba34e4da72bf48acd030bc2a05852
kisatsuki <scarlett1965@inbox.ru>
2021-04-02 13:59:39 +0300

    first commit for all files

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 import dj_database_url
 
 prod_db = dj_database_url.config(conn_max_age=500)
