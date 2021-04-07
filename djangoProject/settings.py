160b65fd79fe83964b84d37cc38e3057aebaf4b8
kisatsuki <scarlett1965@inbox.ru>
2021-04-07 19:59:16 +0300

    first commit for all files

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -125,10 +125,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# import dj_database_url
-#
-# prod_db = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+import dj_database_url
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 #
 # перенести на удаленный сервер
